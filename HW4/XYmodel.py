aa5246263062a163ad1d776843bada7138296ee4
terrenceaalsup <terrenceaalsup@gmail.com>
2019-04-14 11:49:55 -0400

    XY model

---
+++ HW4/XYmodel.py
@@ -0,0 +1,79 @@
+"""
+Author: Terrence Alsup
+Date: April 13, 2019
+Monte Carlo Methods HW 4
+File: XYmodel.py
+
+Implement the XY model as a Python class and include methods to compute the
+magnetization vector as well as the cosine of the angle of the magnetizaion
+vector.  Also contains methods to evaluate the log density and its gradient.
+"""
+import numpy as np
+
+class XYmodel:
+    """
+    An instance of the XY model.
+    """
+    def __init__(self, L, beta):
+        """
+        Initialize the XY model with uniform random angles.
+        """
+        self.L = L          # Lattice size
+        self.beta = beta    # Inverse temperature
+        self.theta = 2*np.pi*np.random.rand(L)  # Uniform random angles.
+
+    def spinVectors(self):
+        """
+        Return an L-by-2 vector of the spins at each lattice site.
+        """
+        spinVec = np.zeros((self.L, 2))
+        spinVec[:,0] = np.cos(self.theta)
+        spinVec[:,1] = np.sin(self.theta)
+        return spinVec
+
+
+    def magnetization(self):
+        """
+        Compute the magnetization vector in R^2.
+        """
+        sigma_x = np.cos(self.theta)
+        sigma_y = np.sin(self.theta)
+        return np.asarray([np.sum(sigma_x), np.sum(sigma_y)])
+
+    def cosMagnetVector(self):
+        """
+        Return the cosine of the angle of the magnetization vector.
+        """
+        mag = self.magnetization()
+        return mag[0]/np.sqrt(mag[0]**2 + mag[1]**2)
+
+    def log_density(self):
+        """
+        Compute the log of the density up to its normalizing constant.
+        beta sum_{i <-> j} cos(theta_i - theta_j)
+        """
+        sum = 0.
+        for i in range(self.L):
+            sum += np.cos(self.theta[np.mod(i+1, self.L)] - self.theta[i])
+
+        return (self.beta * sum)
+
+    def grad_log_density(self):
+        """
+        Return the gradient of log pi w.r.t. theta as a vector of size L.
+        """
+        grad = np.zeros(self.L)
+        for i in range(self.L):
+            grad[i] = np.sin(self.theta[np.mod(i-1, self.L)] - self.theta[i])
+            grad[i] += np.sin(self.theta[np.mod(i+1, self.L)] - self.theta[i])
+
+        return (self.beta * grad)
+
+    def density_unnormalized(self):
+        return np.exp(self.log_density())
+
+    def set(self, new_theta):
+        """
+        Set new_theta as the current vector of thetas.
+        """
+        self.theta = new_theta
---
+++ HW4/ex60_alsup.py
@@ -0,0 +1,146 @@
+import XYmodel
+import numpy as np
+import acor
+from matplotlib import pyplot as plt
+from numpy import linalg
+
+
+
+def sampleXY(L, beta, h, Nsteps, metropolize=False, getMags=False):
+    """
+    Generate a sample from the XY model.
+    Un-metropolized.
+
+    S = I
+    xi ~ N(0, I)
+    """
+    xy = XYmodel.XYmodel(L, beta)
+
+    if getMags:
+        mag = np.zeros(Nsteps+1)
+        mag[0] = xy.cosMagnetVector()
+
+    if metropolize:
+        rejected = 0
+
+    for k in range(1,Nsteps+1):
+
+        hgradLogPi = h * xy.grad_log_density() # Drift term.
+        dX = hgradLogPi + (np.sqrt(h) * np.random.randn(L))
+        Y = dX + xy.theta
+
+        if metropolize:
+            qxy = np.exp(linalg.norm(-dX - hgradLogPi)**2/(4*h))    # q(x|y)
+            qyx = np.exp(linalg.norm( dX - hgradLogPi)**2/(4*h))    # q(y|x)
+            X = xy.theta # The current value of theta.
+            pix = xy.density_unnormalized()
+            xy.set(Y) # Go ahead and set the values of theta.
+            piy = xy.density_unnormalized()
+            p_acc = (qxy * piy)/(qyx * pix) # Acceptance probability.
+            # If rejected, reset to the old value.
+            if np.random.rand() > p_acc:
+                xy.set(X)
+                rejected += 1
+        else:
+            xy.set(Y)
+        if getMags:
+            mag[k] = xy.cosMagnetVector()
+
+    if metropolize:
+        rej_rate = float(rejected)/Nsteps
+
+    if getMags and metropolize:
+        return [xy, rej_rate, mag]
+    elif getMags and not metropolize:
+        return [xy, mag]
+    elif not getMags and metropolize:
+        return [xy, rej_rate]
+    else:
+        return xy
+
+
+def test_sampler():
+    """
+    Test the un-metropolized XY model sampler.
+    """
+    L = 25              # Lattice size
+    beta = 10.0         # Inverse temperature
+    h = 1E-3            # Step size
+    Nsteps = int(1E4)   # Number of MCMC steps
+
+    # First plot the spins on the circle for large beta.
+
+    # Get points on the circle.
+    angle = np.linspace(0, 2*np.pi, 1000)
+    circle = [np.cos(angle), np.sin(angle)]
+
+    # Sample from the model and get the spin vectors.
+    xy = sampleXY(L, beta, h, Nsteps)
+    spins = xy.spinVectors()
+
+    # Now plot the result.
+    plt.figure(1)
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.title("XY model sample ($L = {:d}$ and $\\beta = $ {:0.1f})".format(L, beta))
+    plt.xlabel('$\\vec{\\sigma}_x$')
+    plt.ylabel('$\\vec{\\sigma}_y$')
+    plt.axis('equal')
+    plt.xlim([-1, 1])
+    plt.ylim([-1, 1])
+    plt.plot(spins[:,0], spins[:,1], 'bo')
+    plt.plot(circle[0], circle[1], 'k:')
+
+    # Now plot the spins for small beta.
+    beta = 0.1 # Inverse temperature
+
+    # Sample from the model and get the spin vectors.
+    xy = sampleXY(L, beta, h, Nsteps)
+    spins = xy.spinVectors()
+
+    # Now plot the result.
+    plt.figure(2)
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.title("XY model sample ($L = {:d}$ and $\\beta = $ {:0.1f})".format(L, beta))
+    plt.xlabel('$\\vec{\\sigma}_x$')
+    plt.ylabel('$\\vec{\\sigma}_y$')
+    plt.axis('equal')
+    plt.xlim([-1, 1])
+    plt.ylim([-1, 1])
+    plt.plot(spins[:,0], spins[:,1], 'bo')
+    plt.plot(circle[0], circle[1], 'k:')
+
+
+
+def test_IAC(Nsteps):
+    """
+    Test the un-metropolized XY model sampler and get the ACF and IAC.
+
+    Nsteps is the number of MCMC steps.
+    """
+    L = 25              # Lattice size
+    beta = 1E-2         # Inverse temperature
+    h = 1E-2            # Step size
+
+    [xy, mags] = sampleXY(L, beta, h, Nsteps, getMags=True)
+
+    acf = acor.function(mags)
+
+    # Time for the correlation to first reach 0 (within the tolerance).
+    cor_time = np.where(acf <= 1E-6)[0][0]
+
+    plt.figure(3)
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.xlabel('Lag')
+    plt.ylabel('Autocorrelation')
+    plt.title('ACF of the Un-Metropolized Scheme')
+    plt.plot(np.arange(cor_time+1), acf[:cor_time+1], 'b-')
+
+    tau = acor.acor(mags, maxlag = cor_time)[0]
+    print("\nIAC = {:.1f}\n".format(tau))
+
+
+
+plt.show()
